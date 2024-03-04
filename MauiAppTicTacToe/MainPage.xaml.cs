2115c0659775cd320dae8a192df454ab63327385
Usuario <Usuario@CPD03-M13>
2024-03-04 15:55:19 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTicTacToe/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiAppTicTacToe
+{
+    public partial class MainPage : ContentPage
+    {
+        string turn = "O";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Button trigger = (Button)sender;
+
+            trigger.IsEnabled = false;
+
+            if(turn == "O")
+            {
+                trigger.Text = "O";
+                turn = "X";
+            } else
+            {
+                trigger.Text = "X";
+                turn = "O";
+            }
+        }
+    }
+}
